c7fc47cd623505db3addcab693984b217776c666
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-27 18:48:54 +0300

    done

--- CoreDataFetchRequestBC/ContentView.swift
+++ CoreDataFetchRequestBC/ContentView.swift
@@ -10,67 +10,90 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+        entity: FruitEntity.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \FruitEntity.name, ascending: true)])
+    var fruits: FetchedResults<FruitEntity>
+    
+    @State private var textFieldText = ""
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+            VStack(spacing: 20) {
+                
+                HStack {
+                    TextField("Add fruit here...", text: $textFieldText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .shadow(color: .gray, radius: 5, x: 5, y: 5)
+                    
+                    Button {
+                        addItem()
                     } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                        Text("Submit")
                     }
+                    .buttonStyle(.borderedProminent)
+                    .shadow(color: .gray, radius: 5, x: 5, y: 5)
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                .padding(.horizontal)
+
+                
+                List {
+                    ForEach(fruits) { fruit in
+                        Text(fruit.name ?? "")
+                            .onTapGesture {
+                                updateItem(fruit: fruit)
+                            }
                     }
+                    .onDelete(perform: deleteItems)
                 }
+                .listStyle(PlainListStyle())
+                
             }
-            Text("Select an item")
+            .navigationTitle("Fruits")
         }
     }
-
+    
     private func addItem() {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            let newFruit = FruitEntity(context: viewContext)
+            newFruit.name = textFieldText
+            saveItems()
+            textFieldText = ""
         }
     }
-
+    
+    private func updateItem(fruit: FruitEntity) {
+        withAnimation {
+            let currentName = fruit.name ?? ""
+            let newName = currentName + "!"
+            fruit.name = newName
+            
+            saveItems()
+        }
+    }
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            guard let index = offsets.first else { return }
+            let fruitEntity = fruits[index]
+            viewContext.delete(fruitEntity)
+            
+            //            offsets.map { items[$0] }.forEach(viewContext.delete)
+            saveItems()
         }
+    }
+    
+    private func saveItems() {
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        
     }
 }
 
